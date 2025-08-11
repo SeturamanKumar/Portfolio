5261e8fa8ca82daad685cf6ec6bc5a221b4a13a3
Seturaman Kumar <seturamankumar@gmail.com>
2025-08-11 18:32:09 +0530

    Chore: Prepare Backend For Deployement

--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ require('dotenv').config();
 let Project = require('./models/project.model.js');
 
 const app = express();
-const PORT = 5001;
+const PORT = process.env.PORT || 5001;
 
 app.use(cors());
 app.use(express.json());
