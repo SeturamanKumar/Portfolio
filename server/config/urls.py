3d2b9da2029a8dc0a91a12e06826a93998905d3d
Seturaman Kumar <seturamankumar@gmail.com>
2026-02-15 18:53:28 +0530

    Secured admin URL and added the configs to add run the website

--- server/config/urls.py
+++ server/config/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('super-secret-admin-console/', admin.site.urls),
     path('api/', include('contact.urls')),
 ]
